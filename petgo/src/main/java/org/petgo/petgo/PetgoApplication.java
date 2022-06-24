20660f188165156555ac7a1fe81cd48bcdf4f778
abbyyvan <499965776@qq.com>
2022-06-24 15:53:09 -0400

    s

---
+++ petgo/src/main/java/org/petgo/petgo/PetgoApplication.java
@@ -0,0 +1,13 @@
+package org.petgo.petgo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetgoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PetgoApplication.class, args);
+	}
+
+}
