fad18cf9ebd1f6a45d0a95823acf38eeb2d242d4
abbyyvan <499965776@qq.com>
2022-06-24 16:37:10 -0400

    init petgo with 8 dependencies

---
+++ petgo/src/test/java/org/petgo/jingyuan/petgo/PetgoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.petgo.jingyuan.petgo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetgoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
